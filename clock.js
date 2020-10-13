10ede61c2645203fda6084f9df2b9ec4313cf38c
UnavenGamer <63145235+UnavenGamer@users.noreply.github.com>
2020-10-13 19:59:17 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,4 +1,11 @@
+<canvas id="clock"
 
+  width="400"
+  height="400"
+  style="width: 28%; border: 2px black solid; background-color:#f1f1f1;">
+</canvas>
+
+<script>
   var canvas = document.getElementById("clock");
   var ctx = canvas.getContext("2d");
   var radius = canvas.height / 2;
@@ -10,9 +17,29 @@
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
-    drawFace(ctx, radius);
   }
 
+
+
   function drawFace(ctx, radius) {
-    ctx.Rad
+    var grad;
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "white"
+    ctx.fill();
+
+    grad = ctx.radialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05 );
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, '#fff');
+    grad.addColorStop(1, '#333');
+    ctx.strokeStyle = grad;
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius *0.1, 2 * Math.PI);
+    ctx.fillStyle = "#333";
+    ctx.fill();
   }
+</script>
